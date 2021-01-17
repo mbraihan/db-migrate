d8c2cdcd4cfdccdfa689f80f882fc0ec09fc34a5
Mohammad Raihanul Bashar <12432136+mraihan19@users.noreply.github.com>
2021-01-17 10:49:12 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    sqlConnectionString: "<connection-string-for-your-sql-database-goes-here>", // Insert your connection string here.
+    mongoConnectionString: "mongodb://maxis-admin:Nopass%401234@m148.maxismongo.com:27017/?authSource=admin&replicaSet=rs0&readPreference=primary&ssl=true", // This puts the resulting database in MongoDB running on your local PC.
+    targetDatabaseName: "maxis_office_manager_0101", // Specify the MongoDB database where the data will end up.
+    skip: [
+    ],
+    remapKeys: false // Set this to false if you want to leave table keys as they are, set to true to remap them to MongoDB ObjectId's.
+};
