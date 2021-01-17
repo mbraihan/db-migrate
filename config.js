ac3c7610339bf839e2644400a262dd0845a2e82e
max_mac <maxis_mac01@Maxis-Mac01s-iMac.local>
2021-01-17 11:06:19 +0600

    Table skipping.

--- config.js
+++ config.js
@@ -4,6 +4,12 @@ module.exports = {
     mongoConnectionString: "mongodb://maxis-admin:Nopass%401234@m148.maxismongo.com:27017/?authSource=admin&replicaSet=rs0&readPreference=primary&ssl=true", // This puts the resulting database in MongoDB running on your local PC.
     targetDatabaseName: "maxis_office_manager_0101", // Specify the MongoDB database where the data will end up.
     skip: [
+        "processLog",
+        "userTerritory",
+        "RoleMenu",
+        "snapTable_dump",
+        "snapTable_report",
+        "SocialData"
     ],
     remapKeys: false // Set this to false if you want to leave table keys as they are, set to true to remap them to MongoDB ObjectId's.
 };
