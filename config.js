ddae7f132e4329e6ed2de663f988ba7c8c19a147
max_mac <maxis_mac01@Maxis-Mac01s-iMac.local>
2021-01-18 16:07:09 +0600

    Table skipping .

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     sqlConnectionString: "<connection-string-for-your-sql-database-goes-here>", // Insert your connection string here.
     mongoConnectionString: "mongodb://maxis-admin:Nopass%401234@m148.maxismongo.com:27017/?authSource=admin&replicaSet=rs0&readPreference=primary&ssl=true", // This puts the resulting database in MongoDB running on your local PC.
-    targetDatabaseName: "test_ekyc_demo_20201227", // Specify the MongoDB database where the data will end up.
+    targetDatabaseName: "visitor_manager_merchant_0101", // Specify the MongoDB database where the data will end up.
     skip: [
         "processLog",
         "userTerritory",
