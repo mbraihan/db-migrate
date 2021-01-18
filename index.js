3d067e5b75ec9442aa1253e542a5f6a8646b5080
max_mac <maxis_mac01@Maxis-Mac01s-iMac.local>
2021-01-18 18:10:58 +0600

    Table skipping .

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ async function main () {
 							user	: 'sa'	,
 							password: 'MaxiS2019pAy2017'	,
 							server	: '182.48.80.230'	,
-							database: 'velocity_0_3_0_1_balancereports'	,
+							database: 'velocity_0_3_0_1_20191229'	,
 							requestTimeout		: 90000	,
 							connectionTimeou	: 90000	,
 							options	: {encrypt: false}
