6c11e0a1c6b8d82a17196edbc09c7857f353f874
max_mac <maxis_mac01@Maxis-Mac01s-iMac.local>
2021-01-19 13:11:44 +0600

    Table skipping .

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     sqlConnectionString: "<connection-string-for-your-sql-database-goes-here>", // Insert your connection string here.
     mongoConnectionString: "mongodb://maxis-admin:Nopass%401234@m148.maxismongo.com:27017/?authSource=admin&replicaSet=rs0&readPreference=primary&ssl=true", // This puts the resulting database in MongoDB running on your local PC.
-    targetDatabaseName: "test", // Specify the MongoDB database where the data will end up.
+    targetDatabaseName: "prodsupport", // Specify the MongoDB database where the data will end up.
     skip: [
         "processLog",
         "userTerritory",
@@ -14,5 +14,6 @@ module.exports = {
         "PartnerContainerTypes",
         "Numbers"
     ],
+
     remapKeys: false // Set this to false if you want to leave table keys as they are, set to true to remap them to MongoDB ObjectId's.
 };
